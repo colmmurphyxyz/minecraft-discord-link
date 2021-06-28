a93f4aa33a4af169ced086e381d5e6d4cb08f3bd
Colm Murphy <colmmurphy016@gmail.com>
2021-06-28 18:57:03 +0100

    disable unnecessary CacheFlags in JDABuilder

--- src/main/kotlin/xyz/colmmurphy/d2mc/D2MC.kt
+++ src/main/kotlin/xyz/colmmurphy/d2mc/D2MC.kt
@@ -2,9 +2,11 @@ package xyz.colmmurphy.d2mc
 
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.requests.GatewayIntent
+import net.dv8tion.jda.api.utils.cache.CacheFlag
 import org.bukkit.plugin.java.JavaPlugin
 import java.lang.NullPointerException
 
@@ -41,8 +43,11 @@ class D2MC : JavaPlugin() {
             GatewayIntent.GUILD_MESSAGES,
             GatewayIntent.GUILD_MESSAGE_REACTIONS,
             GatewayIntent.GUILD_WEBHOOKS,
-            GatewayIntent.GUILD_MEMBERS
+            GatewayIntent.GUILD_MEMBERS,
         )
+            .setActivity(Activity.playing("mc.colmmurphy.xyz"))
+            .disableCache(CacheFlag.ACTIVITY, CacheFlag.EMOTE, CacheFlag.VOICE_STATE, CacheFlag.CLIENT_STATUS,
+                CacheFlag.ONLINE_STATUS)
             .build()
             .awaitReady()
         println("[D2MC] successfully logged into discord as ${jda.selfUser.name}#${jda.selfUser.discriminator}")
